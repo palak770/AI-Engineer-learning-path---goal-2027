54b3bf18c9f47ea5bda8ac6fa02537006a4114a4
Palak <palakpandey1603@gmail.com>
2025-08-08 18:15:12 +0530

    Update Datatype.py with latest changes

---
+++ Python Prerequisites/Datatype.py
@@ -0,0 +1,20 @@
+# datatype are the type which tell us about the type of data we are using 
+
+''' 
+Numeric - int, float, complex
+Sequence Type - string, list, tuple
+Mapping Type - dict
+Boolean - bool
+Set Type - set, frozenset
+Binary Types - bytes, bytearray, memoryview
+
+'''
+
+a = 5
+print(type(a))
+
+b = 5.0
+print(type(b))
+
+c = 2 + 4j
+print(type(c))
