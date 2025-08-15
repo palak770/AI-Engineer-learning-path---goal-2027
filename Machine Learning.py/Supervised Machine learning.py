6a1b3cca4caab1e75d3431a7e8c651e03f4111eb
Palak <95370582+palak770@users.noreply.github.com>
2025-08-16 01:22:42 +0530

    Update Supervised Machine learning.py

---
+++ Machine Learning.py/Supervised Machine learning.py
@@ -0,0 +1,28 @@
+"""                                               Supervised Learning 
+supervised learning is a type of machine learning where a model is trained on labeled data—meaning each input is paired with the correct output.
+the model learns by comparing its predictions with the actual answers provided in the training data.
+Over time, it adjusts itself to minimize errors and improve accuracy. 
+The goal of supervised learning is to make accurate predictions when given new, unseen data
+"""
+
+#Supervised machine learning involves training a model on labeled data to learn patterns and relationships, which it then uses to make accurate predictions on new data.
+
+"""                                       it includes 
+1. Training phase( algo): involves feeding the algorithm labeled data, where each data point is paired with its correct output.
+  The algorithm learns to identify patterns and relationships between the input and output data.
+
+2. Testing phase (logic): involves feeding the algorithm new, unseen data and evaluating its ability to predict the correct output based on the learned patterns.
+"""
+
+'''                           Types of Supervised Learning in Machine Learning
+
+Classification(defined labels): Where the output is a categorical variable (e.g., spam vs. non-spam emails, yes vs. no).
+Regression( no defined labels ): Where the output is a continuous variable (e.g., predicting house prices, stock prices).
+
+Classification teaches a machine to sort things into categories. It learns by looking at examples with labels (like emails marked "spam" or "not spam").
+After learning, it can decide which category new items belong to, like identifying if a new email is spam or not.
+'''
+
+
+
+
